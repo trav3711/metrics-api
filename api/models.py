053a78cc2c525d9273e53d71df82de3ad4c7cd13
Travis Welch <trav.cwelch@gmail.com>
2021-01-19 15:54:30 -0800

    update db models

--- api/models.py
+++ api/models.py
@@ -2,18 +2,23 @@ from django.db import models
 from django.contrib.postgres.indexes import BTreeIndex
 from django.contrib.auth.models import User
 
-class Timetypes(models.Model):
-    types = models.CharField(max_length=10)
+TIME_TYPES = [
+    ('daily', 'daily'),
+    ('weekly', 'weekly'),
+    ('bi-weekly', 'bi-weekly'),
+    ('monthly', 'monthly'),
+    ('annually', 'annually')
+]
 
-class Metrics(models.Model):
+class Metric(models.Model):
     metric_name = models.CharField(max_length = 25)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    time_type = models.ForeignKey(Timetypes, on_delete=models.CASCADE)
+    #time_type = models.CharField(choices=TIME_TYPES, default=TIME_TYPES[0], max_length=10)
     def __str__(self):
         return self.metric_name
 
-class Entries(models.Model):
-    metric = models.ForeignKey(Metrics, on_delete=models.CASCADE)
+class Entry(models.Model):
+    metric = models.ForeignKey(Metric, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     entry_time = models.DateTimeField('Entry Time', auto_now=True)
