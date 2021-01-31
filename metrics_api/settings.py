625a491cba4c5531bf4cc111a7d3a74ec1fff62f
Travis Welch <trav.cwelch@gmail.com>
2021-01-31 11:41:34 -0800

    update environment imports

--- metrics_api/settings.py
+++ metrics_api/settings.py
@@ -11,9 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
-
-ALLOWED_HOSTS = ['*']
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
