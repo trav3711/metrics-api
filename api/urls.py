3a0a122ca3c11c9db32105a231d6c080778193f4
Travis Welch <trav.cwelch@gmail.com>
2021-04-04 11:40:45 -0700

    migrate from viewsets to generic class based views with APIView base

--- api/urls.py
+++ api/urls.py
@@ -4,19 +4,13 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 from api import views
 
-entry_list = views.EntryViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-
-# Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'metrics', views.MetricViewSet, 'get_queryset')
-#router.register(r'entries', views.EntryViewSet, 'get_queryset')
 router.register(r'users', views.UserViewSet)
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('metrics/<int:pk>/entries/', entry_list, name='entry-list'),
+    path('metrics/', views.MetricList.as_view()),
+    path('metrics/<int:pk>/', views.MetricDetail.as_view()),
+    path('metrics/<int:fk>/<int:pk>/', views.EntryDetail.as_view()),
     path('', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from api.models import Metric, Entry
-from api.serializers import MetricSerializer, EntrySerializer, UserSerializer
+from api.serializers import *
 #from api.permissions import IsOwnerOrReadOnly
 
+from django.http import Http404
+from rest_framework.views import APIView
 from rest_framework import generics, permissions, status
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -10,47 +12,81 @@ from rest_framework.response import Response
 from rest_flex_fields import is_expanded
 #from rest_framework_simplejwt.views import TokenObtainPairView
 
-class MetricViewSet(viewsets.ModelViewSet):
-    #queryset = Metric.objects.all()
-    serializer_class = MetricSerializer
+class MetricList(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
-        queryset =  Metric.objects.filter(owner=self.request.user.id)
+    def get(self, request):
+        metrics = Metric.objects.filter(owner=request.user)
+        serializer = MetricSerializer(metrics, many=True)
+        return Response(serializer.data)
 
-        if is_expanded(self.request, 'entries'):
-            queryset = queryset.prefetch_related('entries')
+    def post(self, request):
+        serializer = MetricCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        return queryset
-
-    def perform_create(self, serializer):
-        """
-        binds a Metric to a user when created
-        """
-        serializer.save(owner=self.request.user)
-
-class EntryViewSet(viewsets.ModelViewSet):
-    serializer_class = EntrySerializer
+class MetricDetail(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
-        return Entry.objects.filter(owner = self.request.user.id,
-                                    metric = self.kwargs['pk'])
+    def get_object(self, pk):
+        try:
+            return Metric.objects.get(pk=pk)
+        except Metric.DoesNotExist:
+            raise Http404
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        metric = serializer.validated_data['metric']
-        self.perform_create(serializer, metric)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    def get(self, request, pk):
+        metric = self.get_object(pk)
+        serializer = MetricSerializer(metric)
+        return Response(serializer.data)
 
-    def perform_create(self, serializer, metric):
-        """
-        Binds an Entry to a Metricd when created
-        """
-        serializer.save(metric=metric,
-                        owner=self.request.user)
+    def post(self, request):
+        serializer = EntrySerializer(data=request.data)
+        if serialzier.is_valid():
+            serlizer.save(metric=request.metric)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        metric = self.get_object(pk)
+        serializer = MetricCreateSerializer(metric, data=request.data)
+        if serializer.is_valid():
+            serliazer.save(owner=request.user)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        metric = self.get_object(pk)
+        metric.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class EntryDetail(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self, pk, fk):
+        try:
+            return Entry.objects.get(fk=fk, pk=pk)
+        except Entry.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, fk):
+        entry = self.get_object(pk, fk)
+        serliazer = EntrySerializer(entry)
+        return Response(serializer.data)
+
+    def put(self, request, pk, fk):
+        entry = self.get_object(pk, fk)
+        serliazer = EntrySerializer(entry, data=request.data)
+        if serializer.is_valid():
+            serliazer.save(metric=request.metric)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, fk):
+        entry = self.get_object(pk, fk)
+        entry.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
